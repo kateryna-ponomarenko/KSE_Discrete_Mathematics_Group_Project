4b744fd015fd3cfcf6b98b995a3c66787668e1e0
kateryna-ponomarenko <kateryna.ponomarenko.25@kse.org.ua>
2025-12-08 14:56:30 +0200

    Update group_project_algorythm.py
    algorithm ver.2

--- group_project_algorythm.py
+++ group_project_algorythm.py
@@ -12,23 +12,47 @@ def shortest_path(i, j, k):
     )
 
 def print_matrix():
+    dist = [[INF] * v for _ in range(v)]
+    for i in range(v):
+        for j in range(v):
+            if i == j:
+                dist[i][j] = 0
+            elif matrix[i][j] == 1:
+                dist[i][j] = 1
+
+    for k in range(v):
+        for i in range(v):
+            for j in range(v):
+                if dist[i][k] + dist[k][j] < dist[i][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
+
+    print("Матриця найкоротших шляхів: ")
     for row in matrix:
         print(*row)
-
 
 
 with open('math.txt', 'r') as file:
     line = file.readline()
     line_without_spaces = line.replace(' ', '').strip()
-    v = len(line_without_spaces) - 1
+    v = len(line_without_spaces)
 
     matrix = [[random.randint(0, 1) for _ in range(v)] for _ in range(v)]
 
-    for i in range(v):
-        new_line = file.readline()
-        new_line_without_spaces = new_line.replace(' ', '').strip()
-        lst = list(new_line_without_spaces)
-        for j in range(v):
-            matrix[i][j] = lst[j]
+    matrix[0] = [int(ch) for ch in line_without_spaces]
 
-    print_matrix()
+    for i in range(1, v):
+        new_line = file.readline()
+        if not new_line:
+            raise ValueError
+
+        new_line_without_spaces = new_line.replace(' ', '').strip()
+        new_line_without_spaces = new_line_without_spaces[:v]
+
+        lst = [int(ch) for ch in new_line_without_spaces]
+        if len(lst) < v:
+            raise ValueError
+
+        matrix[i] = lst
+
+
+print_matrix()
